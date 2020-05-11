d4deb4efa7ac3eac717aba9de54a3fab63d3ecc8
Yannick Hauser <48750783+YannickHauser@users.noreply.github.com>
2020-05-11 18:13:07 +0200

    Update test_input.py

--- tests/test_input.py
+++ tests/test_input.py
@@ -4,30 +4,46 @@ import os
 import sys
 import pandas as pd
 sys.path.append('../')
+
 import processing.procswi as processing_SWI
 
 
 #import example lethargus data
-lethargus_data = pd.read_csv("../Lethargus_pYPH5_EV.csv")
-gfp_data = pd.read_csv("../Kymograph_Quantification.csv")
+data_path = "C:/Users/hausyann/source/repos/ggrosshans_SWIanalysis/example_files/"
 
-def test_lethargus_intmolts(lethargus_data):
-    intmolts, molts = processing_SWI.lethargus_analysis(lethargus_data)
+#test load_gfp:
+def load_gfp(uploaded_file_GFP):
+    uploaded_file_GFP = st.sidebar.file_uploader("Choose file for GFP data", type="csv")
+    if uploaded_file_GFP is not None:
+        gfpdata_original = pd.read_csv(uploaded_file_GFP)
+        gfpdata = gfpdata_original.pivot("Frame","Position", "Intensity_BGsub")
+        return gfpdata
+
+
+def test_lethargus_intmolts():
+    lethargus = pd.read_csv(data_path + "Lethargus_pYPH5_EV.csv")
+    intmolts, molts = processing_SWI.lethargus_analysis(lethargus)
     assert intmolts[1,1,12] == 128.0
 
-def test_lethargus_molts(lethargus_data):
-    intmolts, molts = processing_SWI.lethargus_analysis(lethargus_data)
+def test_lethargus_molts():
+    lethargus = pd.read_csv(data_path + "Lethargus_pYPH5_EV.csv")
+
+    intmolts, molts = processing_SWI.lethargus_analysis(lethargus)
     assert molts[1,1,12] == 137.0
-"""
-def test_easy():
-    a = 5
-    b = 3
-    assert a + b == 8
 
 
-def func(x):
-    return x + 1
+def test_lethargus_length():
+    lethargus = pd.read_csv(data_path + "Lethargus_pYPH5_EV.csv")
 
-def test_answer():
-    assert func(3) == 4
-"""
+    intmolts, molts = processing_SWI.lethargus_analysis(lethargus)
+    assert len(intmolts[1,0,:]) == 16
+
+def test_gfp_adjusting():
+    gfpdata = pd.read_csv(data_path + "Kymograph_Quantification.csv")
+    lethargus = pd.read_csv(data_path + "Lethargus_pYPH5_EV.csv")
+    intmolts, molts = processing_SWI.lethargus_analysis(lethargus)
+    dev_length = 231
+
+    gfp_adjusted = adjust_gfp(gfpdata, intmolts, dev_length)
+    assert 
+
