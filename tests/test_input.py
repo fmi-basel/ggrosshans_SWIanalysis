64a865358083f6139cfb8da7c799a0a4293eb773
Yannick Hauser <48750783+YannickHauser@users.noreply.github.com>
2020-05-11 17:14:55 +0200

    Update test_input.py

--- tests/test_input.py
+++ tests/test_input.py
@@ -6,9 +6,10 @@ import pandas as pd
 sys.path.append('../')
 import processing.procswi as processing_SWI
 
+
 #import example lethargus data
-lethargus_data = pd.read_csv("../example_files/Lethargus_pYPH5_EV.csv")
-gfp_data = pd.read_csv("../example_files/Kymograph_Quantification.csv")
+lethargus_data = pd.read_csv("../Lethargus_pYPH5_EV.csv")
+gfp_data = pd.read_csv("../Kymograph_Quantification.csv")
 
 def test_lethargus_intmolts(lethargus_data):
     intmolts, molts = processing_SWI.lethargus_analysis(lethargus_data)
@@ -17,4 +18,16 @@ def test_lethargus_intmolts(lethargus_data):
 def test_lethargus_molts(lethargus_data):
     intmolts, molts = processing_SWI.lethargus_analysis(lethargus_data)
     assert molts[1,1,12] == 137.0
+"""
+def test_easy():
+    a = 5
+    b = 3
+    assert a + b == 8
 
+
+def func(x):
+    return x + 1
+
+def test_answer():
+    assert func(3) == 4
+"""
