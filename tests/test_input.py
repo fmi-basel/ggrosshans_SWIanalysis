de0c73dd980d0404807a1c10fffcf3aa2252e2fd
Yannick Hauser <48750783+YannickHauser@users.noreply.github.com>
2020-05-11 14:50:15 +0200

    Update test_input.py

--- tests/test_input.py
+++ tests/test_input.py
@@ -1,1 +1,15 @@
-#test1
+#tests for SWI
+import pytest
+import os
+import sys
+from processing.procswi import *
+#prepare fake lethargus data
+lethargus_data = pd.DataFrame
+
+
+
+class Test_class:
+    def test_lethargus_input(lethargus_data):
+        
+
+
