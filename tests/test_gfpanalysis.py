e190e3d260cd030037b08ea0ddd71b15634cae72
Yannick Hauser <48750783+YannickHauser@users.noreply.github.com>
2020-05-13 18:06:40 +0200

    lethargus_testcase added (not yet completed)

--- tests/test_gfpanalysis.py
+++ tests/test_gfpanalysis.py
@@ -10,7 +10,21 @@ import processing.procswi as processing_SWI
 #import example lethargus data
 DATA_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'example_files/')
 
-lethargus = pd.read_csv(DATA_PATH + "Lethargus_pYPH5_EV.csv")
+def lethargus_testcase():
+    '''Utility to load the test case. Could be extended to load
+    not only one but several test cases.
+    '''
+    return pd.read_csv(DATA_PATH + "Lethargus_pYPH5_EV.csv")
+  
+@pytest.fixture
+def gfp_testcase():
+    '''Utility to load the test case. Could be extended to load
+    not only one but several test cases.
+    '''
+    return pd.read_csv(DATA_PATH + "Kymograph_Quantification.csv")
+
+
+lethargus = lethargus_testcase()
 intmolts, molts = processing_SWI.lethargus_analysis(lethargus)
 
 dev_length = 231
@@ -19,6 +33,10 @@ gfpdata_original = pd.read_csv(DATA_PATH + "Kymograph_Quantification.csv")
 gfpdata = gfpdata_original.pivot("Frame","Position", "Intensity_BGsub")
 gfp_adjusted = processing_SWI.adjust_gfp(gfpdata, intmolts, dev_length)
 
+#set up f_clean for test_run_hilbert(). This can run correctly if test_interpolation runs correctly
+f_clean = processing_SWI.interpolate_gfp(gfpdata, intmolts, dev_length)
+f_clean_df = pd.DataFrame(f_clean).T
+f_clean_df_clean = f_clean_df.loc[:,:] #normally, in the processing, valid worms are selected
 #tests
 def test_interpolation():
     f_clean = processing_SWI.interpolate_gfp(gfpdata, intmolts, dev_length)
@@ -33,10 +51,7 @@ def test_calculate_durations():
                                                                             intmolts, molts)
     assert np.sum(molt_dur["variable"].unique() == ['M1', 'M2', 'M3', 'M4']) == 4
 
-#set up f_clean for test_run_hilbert(). This can run correctly if test_interpolation runs correctly
-f_clean = processing_SWI.interpolate_gfp(gfpdata, intmolts, dev_length)
-f_clean_df = pd.DataFrame(f_clean).T
-f_clean_df_clean = f_clean_df.loc[:,:] #normally, in the processing, valid worms are selected
+
 
 
 def test_run_hilbert():
@@ -49,3 +64,4 @@ def test_run_hilbert():
                                                         fs, 
                                                         dev_length)
     assert np.max(my_phase) <= np.pi and np.min(my_phase) >= -np.pi
+    assert len(my_phase) 
